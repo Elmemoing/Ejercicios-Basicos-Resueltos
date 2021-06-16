a0468e3709c85a30976715b9a264d1ad5a680692
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-06-15 20:06:04 -0400

    Update Ejercicio.cpp

--- Ejercicio.cpp
+++ Ejercicio.cpp
@@ -1,8 +1,6 @@
 #include <conio.h>
 #include <stdio.h>
 
-
-
 #define MAX 50
 
 int main()
@@ -18,19 +16,19 @@ int main()
 	
 	/*Calculo de Longuitud*/
 	do
-   {
+        {
 	i++;
 	longitud=i;
-   }while((frase[i]!='\0')&&(i<MAX+1)); 
+       }while((frase[i]!='\0')&&(i<MAX+1)); 
 
-    /*Ejercicio numero 2*/
-    if (frase[0]== 'A' || frase[0]== 'E' || frase[0]== 'I' || frase[0]== 'O' || frase[0]== 'U')
-    {
-    printf ("\n El texto ingresado de forma inversa es: ");
-    for (int x = longitud; (x >= 0); x--)
-    {
-      printf ("%c", frase[x]);
-    }
-    } else{
-    	printf("No comienza con vocal mayuscula \n");
-	}
+       /*Ejercicio numero 2*/
+       if (frase[0]== 'A' || frase[0]== 'E' || frase[0]== 'I' || frase[0]== 'O' || frase[0]== 'U')
+       {
+       printf ("\n El texto ingresado de forma inversa es: ");
+       for (int x = longitud; (x >= 0); x--)
+       {
+       printf ("%c", frase[x]);
+       }
+       } else{
+       printf("No comienza con vocal mayuscula \n");
+       }
