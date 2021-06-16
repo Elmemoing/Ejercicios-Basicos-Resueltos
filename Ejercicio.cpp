3c24f58a37de8c34c5a6be7a4f01841fe20ad95b
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-06-15 21:13:23 -0400

    Update Ejercicio.cpp

--- Ejercicio.cpp
+++ Ejercicio.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-	char frase[MAX];
+	char frase[MAX], mayuscula[MAX];
 	int longitud=0, i=0, x=0;
 	printf("Ingrese frase ");
 	gets(frase);
@@ -16,19 +16,51 @@ int main()
 	
 	/*Calculo de Longuitud*/
 	do
-        {
+   {
 	i++;
 	longitud=i;
-       }while((frase[i]!='\0')&&(i<MAX+1)); 
+   }while((frase[i]!='\0')&&(i<MAX+1)); 
 
-       /*Ejercicio numero 2*/
-       if (frase[0]== 'A' || frase[0]== 'E' || frase[0]== 'I' || frase[0]== 'O' || frase[0]== 'U')
-       {
-       printf ("\n El texto ingresado de forma inversa es: ");
-       for (int x = longitud; (x >= 0); x--)
-       {
-       printf ("%c", frase[x]);
-       }
-       } else{
-       printf("No comienza con vocal mayuscula \n");
-       }
+    /*Ejercicio numero 2*/
+    if (frase[0]== 'A' || frase[0]== 'E' || frase[0]== 'I' || frase[0]== 'O' || frase[0]== 'U')
+    {
+    printf ("\n El texto ingresado de forma inversa es: ");
+    for (int x = longitud; (x >= 0); x--)
+    {
+      printf ("%c", frase[x]);
+    }
+    /*Ejercico numero 3*/
+    } else if (frase[x] !='a' && frase[x] !='e' && frase[x] !='i' && frase[x] !='o' && frase[x] !='u'){
+	
+	printf("\npalabra cambiada por mayusculas es:	");
+	x = 0;
+	while(frase[x] != '\0'){
+		mayuscula[x] = frase[x];
+		if (frase[x] !='a' && frase[x] !='e' && frase[x] !='i' && frase[x] !='o' && frase[x] !='u' && frase[x] !='A' && frase[x] !='E' &&
+		frase[x] !='I' && frase[x] !='O' && frase[x] !='U'){
+			if (frase[x] > 97 && frase[x] < 123){
+			
+			mayuscula[x] = frase[x] - 32;
+		}
+		printf("%c",mayuscula[x]);
+		}
+		x += 1;
+	}
+}
+    /*Ejercicio 4*/
+	x = 0;
+	printf("\nPalabra Cambiada por * es:	");	
+	while(frase[x] != '\0'){
+		
+		if (frase[x] !='a' && frase[x] !='e' && frase[x] !='i' && frase[x] !='o' && frase[x] !='u' && frase[x] !='A' && frase[x] !='E' &&
+		frase[x] !='I' && frase[x] !='O' && frase[x] !='U' && frase[x] > 63 && frase[x] < 91){
+			frase[x] = '*';
+		}
+		x += 1;
+	}	
+	
+	printf("%s",frase);
+		
+    getch();
+    return 0;
+}
